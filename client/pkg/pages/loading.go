1aed839bf2185f3a3cacef8f754c0357f54243d1
RGood <mullan17@gmail.com>
2022-10-25 00:11:16 -0400

    Add colors to loading spinner

--- client/pkg/pages/loading.go
+++ client/pkg/pages/loading.go
@@ -14,6 +14,32 @@ type LoadingPage struct {
 	size     int
 	period   time.Duration
 	lastTime time.Time
+	r, g, b  *oscilator
+	adding   bool
+}
+
+type oscilator struct {
+	val       uint16
+	period    time.Duration
+	startTime time.Time
+}
+
+func NewOscilator(period time.Duration) *oscilator {
+	return &oscilator{
+		val:       0,
+		period:    period,
+		startTime: time.Now(),
+	}
+}
+
+func (o *oscilator) increment() {
+	diff := time.Since(o.startTime)
+	count := int(diff / o.period)
+	if count%2 == 0 {
+		o.val = uint16(float64(0xFFFF) * float64(diff%o.period) / float64(o.period))
+	} else {
+		o.val = 0xFFFF - uint16(float64(0xFFFF)*float64(diff%o.period)/float64(o.period))
+	}
 }
 
 func NewLoadingPage(size int, period time.Duration) *LoadingPage {
@@ -22,12 +48,24 @@ func NewLoadingPage(size int, period time.Duration) *LoadingPage {
 		size:     size,
 		period:   period,
 		lastTime: time.Now(),
+		r:        NewOscilator(time.Second * 1),
+		g:        NewOscilator(time.Second * 3),
+		b:        NewOscilator(time.Second * 5),
+		adding:   true,
 	}
 }
 
+var counter = 0
+
 func (lp *LoadingPage) Update() error {
+	counter++
 	lp.rotation += (float64(2*math.Pi) * float64(time.Since(lp.lastTime)) / float64(lp.period))
 	lp.lastTime = time.Now()
+
+	lp.r.increment()
+	lp.g.increment()
+	lp.b.increment()
+
 	return nil
 }
 
@@ -36,7 +74,7 @@ func (lp *LoadingPage) Draw(screen *ebiten.Image) {
 	x, y := ebiten.WindowSize()
 
 	spinner := ebiten.NewImage(lp.size, lp.size)
-	spinner.Fill(color.RGBA64{0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF})
+	spinner.Fill(color.RGBA64{lp.r.val, lp.g.val, lp.b.val, 0xFFFF})
 
 	opts := &ebiten.DrawImageOptions{}
 	opts.GeoM.Translate(float64(-lp.size/2), float64(-lp.size/2))
